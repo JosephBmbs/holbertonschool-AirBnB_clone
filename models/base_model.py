f619e0a03ff34ea88d3522bd727ae3126487da02
Youssef_Saad <6270@holbertonstudents.com>
2023-08-03 19:58:23 +0100

    just for test

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,6 @@
-#!/usr/bin/python3
-"""Defines the BaseModel class."""
+# models/base_model.py
 from uuid import uuid4
 from datetime import datetime
-import models
-
 
 class BaseModel:
     """Represents the BaseModel of the HBnB project."""
@@ -25,13 +22,10 @@ class BaseModel:
                     self.__dict__[k] = datetime.strptime(v, tform)
                 else:
                     self.__dict__[k] = v
-        else:
-            models.storage.new(self)
 
     def save(self):
         """Update updated_at with the current datetime."""
         self.updated_at = datetime.today()
-        models.storage.save()
 
     def to_dict(self):
         """Return the dictionary of the BaseModel instance.
